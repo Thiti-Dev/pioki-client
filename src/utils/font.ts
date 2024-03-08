a762d3c664c33ba06908d687ddabd9eedc23cc01
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-03-08 10:41:10 +0700

    chore: export DancingScript,Workbench fonts for later usage

---
+++ src/utils/font.ts
@@ -0,0 +1,4 @@
+import { Dancing_Script } from 'next/font/google';
+import localFont from 'next/font/local'
+export const dancingScript = Dancing_Script({ subsets: ['latin'] });
+export const workBench = localFont({src: '../fonts/Workbench-Regular-VariableFont_BLED,SCAN.ttf'})
