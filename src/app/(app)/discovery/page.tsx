f24b1d2cf54fe20194d91e2b197849fafb6bc7a7
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-04-05 22:52:16 +0700

    creates empty discovery page

---
+++ src/app/(app)/discovery/page.tsx
@@ -0,0 +1,9 @@
+import ProfileTemplate from "@/components/common/profile-template";
+
+export default async function DiscoveryPage(){
+    return <>
+        <ProfileTemplate>
+            
+        </ProfileTemplate>
+    </>
+}
